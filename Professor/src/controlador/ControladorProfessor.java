1b1bd4ed579a6421cc18143a83c69435f58f8add
Administrador <Administrador@10.10.4.1>
2019-07-01 17:46:52 -0300

    vvvv

---
+++ Professor/src/controlador/ControladorProfessor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import tela.manutencao.ManutencaoProfessor;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ControladorProfessor {
+
+    public static void inserir(ManutencaoProfessor aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
